9d3f63212aac018ed058373a99bf25c52e6cc15e
Tristan Engst <tme28@cornell.edu>
2021-02-26 14:17:01 -0500

    Adding additinal graph

--- a1_search/ProvidedFunctions.py
+++ a1_search/ProvidedFunctions.py
@@ -67,7 +67,7 @@ class Node:
 
     def __repr__(self): return self.__str__()
 
-    def __str__(self): return str_(self, root=True)
+    def __str__(self): return print_node(self, root=True)
         
     def __eq__(self, other): return str_(self) == str(other)
     
