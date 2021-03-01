fc749276bba4e913de0ce4446ad84d1c224d8d77
Tristan Engst <tme28@cornell.edu>
2021-03-01 13:08:28 -0500

    Undoing previous change

--- a1_search/ProvidedFunctions.py
+++ a1_search/ProvidedFunctions.py
@@ -178,10 +178,8 @@ def get_successor(state, n_successors=10):
     
     grad = w.grad.numpy().reshape(15)
     grad_norm = np.linalg.norm(grad)
-    successors = [state - grad + (np.random.rand(15) * grad_norm) for _ in range(n_successors)]
-    successors += [state + grad + (np.random.rand(15) * grad_norm) for _ in range(n_successors)]
-    return successors
-    
+    return [state - grad + (np.random.rand(15) * grad_norm) for _ in range(n_successors)]
+
 def get_initial_state():
     """Returns an initial state."""
     return np.zeros(15)
